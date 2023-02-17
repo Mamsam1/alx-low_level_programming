d06f97202e8c6ac706c6b470e9412ab908f44997
Mamsam1 <mariamviolasadick@gmail.com>
2023-02-17 14:37:50 -0800

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char s;
+
+	int t;
+
+	s = 'a';
+	t = 0;
+	while
+		(t < 10) {
+			putchar(t + 0);
+			t++;
+		}
+	while
+		(s <= 'v') {
+			putchar(s);
+			s++;
+		}
+	putchar('\n');
+	return (0);
+}
